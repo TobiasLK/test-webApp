5b0472059dda231cefe9a34f9423ea3c50041452
TobiasLK <tobiaslindknudsen@gmail.com>
2023-05-13 16:21:46 +0200

    Adding downloads to start file

--- start.sh
+++ start.sh
@@ -2,4 +2,11 @@
 
 python -m spacy download en_core_web_sm
 
+python -m nltk.downloader punkt
+python -m nltk.downloader averaged_perceptron_tagger
+python -m nltk.downloader vader_lexicon
+python -m nltk.downloader stopwords
+python -m nltk.downloader wordnet
+python -m nltk.downloader omw-1.4
+
 exec streamlit run BA_App6.py
